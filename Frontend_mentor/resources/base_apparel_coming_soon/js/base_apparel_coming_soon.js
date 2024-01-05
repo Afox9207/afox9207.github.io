9ed7489d1169127eff1d7fd4a330806b0a83cc18
Afox9207 <127136639+Afox9207@users.noreply.github.com>
2024-01-05 12:32:01 -0500

    add signup form

--- Frontend_mentor/resources/base_apparel_coming_soon/js/base_apparel_coming_soon.js
+++ Frontend_mentor/resources/base_apparel_coming_soon/js/base_apparel_coming_soon.js
@@ -3,10 +3,10 @@ const formBtn = document.querySelector(".form__btn");
 const formErrorIcon = document.querySelector(".form__error-icon");
 const formErrorMessage = document.querySelector(".form__error-message");
 
-formInput.addEventListener("keydown", validateEmail);
+formInput.addEventListener("keyup", validateEmail);
 
 function validateEmail() {
-    if (!formInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    if (!formInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/) && formInput.value) {
         formErrorMessage.style.visibility = "visible";
         formErrorIcon.style.visibility = "visible";
         return false;

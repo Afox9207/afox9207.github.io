ccea3d8d7d592bf3ce32397a0739ac29328de5f4
Afox9207 <127136639+Afox9207@users.noreply.github.com>
2024-01-24 14:06:46 -0500

    Update age_calculator_app.js

--- New Frontend Mentor/resources/age_calculator_app/js/age_calculator_app.js
+++ New Frontend Mentor/resources/age_calculator_app/js/age_calculator_app.js
@@ -1,3 +1,11 @@
+const birthDayInput = document.getElementById("day");
+const birthMonthInput = document.getElementById("month");
+const birthYearInput = document.getElementById("year");
+
+const errorMessageDay = document.getElementById("error-message-day");
+const errorMessageMonth = document.getElementById("error-message-month");
+const errorMessageYear = document.getElementById("error-message-year");
+
 const btn = document.getElementById("btn");
 
 const yearResult = document.getElementById("result-years");
@@ -5,11 +13,21 @@ const monthResult = document.getElementById("result-months");
 const dayResult = document.getElementById("result-days");
 
 btn.addEventListener("click", function() {
-    
-    // user input
-    const birthDay = document.getElementById("day").value;
-    const birthMonth = document.getElementById("month").value;
-    const birthYear = document.getElementById("year").value;
+
+    // reset styles
+    birthDayInput.classList.remove("age-calculator__input--error");
+    birthMonthInput.classList.remove("age-calculator__input--error");
+    birthYearInput.classList.remove("age-calculator__input--error");
+
+    errorMessageDay.innerText = "";
+    errorMessageMonth.innerText = "";
+    errorMessageYear.innerText = "";
+
+    // get user input and convert it to a number
+    const birthDay = parseInt(birthDayInput.value, 10);
+    const birthMonth = parseInt(birthMonthInput.value, 10);
+    const birthYear = parseInt(birthYearInput.value, 10);
+
 
     // get current date and find difference
     const currentDate = new Date();
@@ -21,28 +39,91 @@ btn.addEventListener("click", function() {
     let monthDifference = currentMonth - birthMonth;
     let yearDifference = currentYear - birthYear;
 
-    // calculate age
-    if (yearDifference < 0) {
-        
-    } else if (monthDifference > 0) {
-        
-    } else if (monthDifference === 0 && dayDifference >= 0) {
+    console.log(yearDifference);
 
-    } else {
-        yearDifference -= 1;
-        if (monthDifference <= 0) {
-            if (dayDifference > 0) {
-                monthDifference += 12;
+    // validate user input
+    
+    // check if values exist
+    if (birthDay && birthMonth && birthYear) {
+        // check if inputs are in range
+        if (birthDay < 1 || birthDay > 31) {
+            birthDayInput.classList.add("age-calculator__input--error");
+            errorMessageDay.innerText = "Must be a valid day";
+        }
+        if (birthMonth < 1 || birthMonth > 12) {
+            birthMonthInput.classList.add("age-calculator__input--error");
+            errorMessageMonth.innerText = "Must be a valid month";
+        }
+        // check if date is in past 
+        if (yearDifference < 0) {
+            birthYearInput.classList.add("age-calculator__input--error");
+            birthMonthInput.classList.add("age-calculator__input--error");
+            birthDayInput.classList.add("age-calculator__input--error");
+            errorMessageYear.innerText = "Must be in the past";
+        } else if (yearDifference = 0 && monthDifference < 0) {
+            birthYearInput.classList.add("age-calculator__input--error");
+            birthMonthInput.classList.add("age-calculator__input--error");
+            birthDayInput.classList.add("age-calculator__input--error");
+            errorMessageYear.innerText = "Must be in the past";
+        } else if (yearDifference === 0 && monthDifference === 0 && dayDifference < 0) {
+            birthYearInput.classList.add("age-calculator__input--error");
+            birthMonthInput.classList.add("age-calculator__input--error");
+            birthDayInput.classList.add("age-calculator__input--error");
+            errorMessageYear.innerText = "Must be in the past";
+        }
+        // check if date is valid 
+        if (birthMonth === 2) {
+            // check if it's a leap year
+            // must be divisible by 4
+            if (birthYear % 4 === 0) {
+                // if it a century birth year it must be divisible by 400
+                if (birthYear % 100 === 0 && birthYear % 400 !== 0) {
+                    if (birthDay > 28) {
+                        birthDayInput.classList.add("age-calculator__input--error");
+                        errorMessageDay.innerText = "Must be a valid date";
+                    }
+                } else {
+                    if (birthDay > 29) {
+                        birthDayInput.classList.add("age-calculator__input--error");
+                        errorMessageDay.innerText = "Must be a valid date";
+                    }
+                }
             } else {
-                monthDifference = 11 - monthDifference
+                if (birthDay > 28) {
+                    birthDayInput.classList.add("age-calculator__input--error");
+                    errorMessageDay.innerText = "Must be a valid date";
+                }
             }
+        } else if (birthMonth === 4 || birthMonth === 6 || birthMonth === 9 || birthMonth === 11) {
+            if (birthDay > 30) {
+                birthDayInput.classList.add("age-calculator__input--error");
+                errorMessageDay.innerText = "Must be a valid date";
+            }
+        } else {
+            if (birthDay > 31) {
+                birthDayInput.classList.add("age-calculator__input--error");
+                errorMessageDay.innerText = "Must be a valid date";
+            }
+        }
+        
+    } else {
+        // if inputs don't exist
+        if (!birthDay) {
+            birthDayInput.classList.add("age-calculator__input--error");
+            errorMessageDay.innerText = "This field is required";
+        }
+        if (!birthMonth) {
+            birthMonthInput.classList.add("age-calculator__input--error");
+            errorMessageMonth.innerText = "This field is required";
+        }
+        if (!birthYear) {
+            birthYearInput.classList.add("age-calculator__input--error");
+            errorMessageYear.innerText = "This field is required";
         }
     }
 
-    if (dayDifference < 0) {
-        dayDifference += 30;
-        monthDifference -= 1;
-    }
+    // calculate age
+    // get years
 
     // display on screen
     yearResult.innerText = yearDifference;
@@ -50,17 +131,12 @@ btn.addEventListener("click", function() {
     dayResult.innerText = dayDifference;
 
     // logs
+    console.log("birth day: " + birthDay);
+    console.log("birth month: " + birthMonth);
+    console.log("birth year: " + birthYear);
 
-    // console.log("currentYear: " + currentYear);
-    // console.log("currentMonth: " + currentMonth);
-    // console.log("currentDay: " + currentDay);
-
-    // console.log("birthYear: " + birthYear);
-    // console.log("birthMonth: " + birthMonth);
-    // console.log("birthDay: " + birthDay);
-
-    // console.log("yearDifference: " + yearDifference);
-    // console.log("monthDifference: " + monthDifference);
-    // console.log("dayDifference: " + dayDifference);
-
+    console.log("year difference: " + yearDifference);
+    console.log("month difference: " + monthDifference);
+    console.log("day difference: " + dayDifference);
 });
+
